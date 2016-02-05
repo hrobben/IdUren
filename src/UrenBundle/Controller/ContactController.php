f4d359a64401e4a6f3930c81fe297fa4a5bfdcc0
hrobben <hrobben@robusoft.nl>
2016-02-05 15:43:04 +0100

    first commit

--- src/UrenBundle/Controller/ContactController.php
+++ src/UrenBundle/Controller/ContactController.php
@@ -20,7 +20,21 @@ class ContactController extends Controller
         $form = $this->createForm(EnquiryType::class, $enquiry);
         $form->handleRequest($request);
         if ($form->isSubmitted()) {
-          var_dump($form);
+            // Perform some action, such as sending an email
+            $message = \Swift_Message::newInstance()
+                ->setSubject('Contact enquiry from uren')
+                ->setFrom('hrobben@robusoft.com')
+               // ->setTo($this->container->getParameter('uren.emails.contact_email'))
+                ->setTo('hrobben@robusoft.nl')
+                ->setBody($this->renderView('UrenBundle:Contact:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+            $this->get('mailer')->send($message);
+
+//                $this->get('session')->setFlash('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
+            $this->get('session')->getFlashBag()->add('uren-notice', 'Your contact enquiry was successfully sent. Thank you!');
+
+            // Redirect - This is important to prevent users re-posting
+            // the form if they refresh the page
+            return $this->redirect($this->generateUrl('uren_contact_contact'));
         }
 
         return $this->render('UrenBundle:Contact:contact.html.twig', [
--- src/UrenBundle/Entity/Enquiry.php
+++ src/UrenBundle/Entity/Enquiry.php
@@ -3,6 +3,9 @@
 
 namespace UrenBundle\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
 class Enquiry
 {
     protected $name;
